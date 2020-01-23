49d1b756fcf023c0d0c97dd5a7cfc002b4dd6cfc
Daniel Crawford <danielpkcrawford@gmail.com>
2020-01-22 16:48:43 -0800

    changed makefile and dir locations

---
+++ app/inc/version.h
@@ -0,0 +1,13 @@
+#ifndef _VERSION_H_
+#define _VERSION_H_
+ 
+// Git URL
+#define GIT_URL "https://github.com/crawfdan/stm.git"
+ 
+// Git Branch
+#define GIT_BRANCH "master"
+ 
+// Simplexity Version
+#define VERSION "1912182ddebf"
+ 
+#endif // _VERSION_H_
