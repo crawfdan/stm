f6a7b50d245f0f91466c2867610aa59a87e25792
Daniel Crawford <danielpkcrawford@gmail.com>
2020-02-19 17:34:23 -0800

    Added .gitignore and removed obj folder from git commits

--- build/obj_nucleo207/startup_stm32f207xx.d
+++
@@ -1,2 +0,0 @@
-/mnt/c/Users/Daniel/Documents/stm/build/obj_nucleo207/startup_stm32f207xx.o: \
- /mnt/c/Users/Daniel/Documents/stm/ThirdParty/nucleo207/Drivers/CMSIS/Device/ST/STM32F2xx/Source/Templates/gcc/startup_stm32f207xx.s
