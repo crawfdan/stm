a620ba1228493916f5856dac4fc7f8378540cd5b
Daniel Crawford <danielpkcrawford@gmail.com>
2020-03-03 14:48:03 -0800

    Updated functions for better task management
    Added led_On, led_Off, led_ColorChange, led_Blink, and modified led_activate subroutines

--- app/Drivers/Src/led.c
+++ app/Drivers/Src/led.c
@@ -19,34 +19,43 @@ typedef enum {
     ledColors_White           = ledColors_Red | ledColors_Green | ledColors_Blue
 } ledColors_t;
 
+//current given behavior for led collection
 typedef enum {
-    ledBehavior_Drive,
+    ledBehavior_Drive = 0x0,
     ledBehavior_Blinking 
 } ledBehavior_t;
 
+//current state of the led collection
 typedef enum {
-    ledCollectionState_Off,
+    ledCollectionState_Off = 0x0,
     ledCollectionState_On
 } ledCollectionState_t;
 
+typedef enum {
+    invalidCollection = -1,
+    collection1
+} ledCollections_t;
+
+//single led structure
 typedef struct led {
     uint16_t pin; //GPIO Pin of led
     GPIO_TypeDef *port; //GPIO port of led
 } led_t;
 
-//NOTE may need larger values for count/thresh
+//data stored for blinking led collection
 typedef struct blinkInfo {
-    int blinkCount;
-    int blinkThreshold; 
-    int numBlinks;
-} BlinkInfo_t;
+    long blinkCount; //current number of cycles to blink (duration)
+    long blinkThreshold; //max number of cycles to blink
+    int numBlinks; //how many times the led will blink
+} blinkInfo_t;
 
+//struct for collection of rgb leds
 typedef struct collection {
-    led_t leds[NUM_LEDS];
-    ledColors_t color;
-    BlinkInfo_t blink;
-    ledCollectionState_t state;
-    ledBehavior_t behavior; 
+    led_t leds[NUM_LEDS]; //leds in the collection
+    ledColors_t color; //current color of collection
+    blinkInfo_t blink; //blink info for collection
+    ledCollectionState_t state; //current on/off state of collection
+    ledBehavior_t behavior; //driving or blinking
 } Collection_t;
 
 
@@ -78,14 +87,14 @@ static Collection_t collArray[NUM_COLLS] = {
         .leds = {
                     [0] = 
                     {
-                        .pin = ledG_Pin,
-                        .port = ledG_GPIO_Port
+                        .pin = ledR_Pin,
+                        .port = ledR_GPIO_Port
                     },
 
                     [1] = 
                     {
-                        .pin = ledR_Pin,
-                        .port = ledR_GPIO_Port
+                        .pin = ledG_Pin,
+                        .port = ledG_GPIO_Port
                     },
 
                     [2] = 
@@ -95,53 +104,97 @@ static Collection_t collArray[NUM_COLLS] = {
                     }
                 },
         .color = ledColors_White,
-        .blink = {.blinkCount = 0, .blinkThreshold = BLINK_THRESHOLD, .numBlinks = 10}
+        .blink = {.blinkCount = 0, .blinkThreshold = BLINK_THRESHOLD, .numBlinks = 10},
+        .state = ledCollectionState_Off,
+        .behavior = ledBehavior_Drive
     }
 };
 
 
 //turn on the led indefinitely
-void led_activateLed(Collection_t coll)
+void led_On(Collection_t coll)
 {
     int i;
+    unsigned mask = 0x1;
     for(i = 0; i < NUM_LEDS; i++)
     {
-        if ()
+        //if there is a 1 in the bit position of the color, turn on that pin
+        if ((coll.color & mask) > 0)
         {
-            HAL_GPIO_WritePin(coll.leds[], GPIO_PIN_SET);
+            HAL_GPIO_WritePin(coll.leds[i].port, coll.leds[i].pin, SET);
         }
+
+        //shift mask for next bit in color
+        mask <<= 1;
     }
-    
+    coll.state = ledCollectionState_On;
+    return;
+}
+
+//turns off leds in a collection indefinitely
+void led_Off (Collection_t coll)
+{
+    int i;
+    //set all leds to off state
+    for(i = 0; i < NUM_LEDS; i++)
+    {
+        HAL_GPIO_WritePin(coll.leds[i].port, coll.leds[i].pin, RESET);
+    }
+    //update states
+    coll.state = ledCollectionState_Off;
+    return;
+}
+
+void led_ColorChange (Collection_t coll, ledColors_t newColor)
+{
+    assert(newColor >= ledColors_Red);
+    coll.color = newColor;
+    return;
+}
+
+void led_Blink (Collection_t coll)
+{
+    //if we meet the threshold, toggle the pin
+    if (coll.blink.blinkCount == coll.blink.blinkThreshold)
+    {
+        if (coll.state == ledCollectionState_Off)
+        {
+            led_On(coll);
+            coll.blink.numBlinks++;
+        }
+
+        else
+        {
+            led_Off(coll);
+        }
+        
+    }
+
+    else
+    {
+        coll.blink.blinkCount++;
+    }
+
     return;
 }
 
 void led_activateRedLed()
 {
-    led_activateLed(ledColors_Red);
+    led_ColorChange(collArray[collection1], ledColors_Red);
+    led_On(collArray[collection1]);
     return;
 }
 
 void led_activateBlueLed()
 {
-    led_activateLed(ledColors_Blue);
+    led_ColorChange(collArray[collection1], ledColors_Blue);
+    led_On(collArray[collection1]);
     return;
 }
 
 void led_activateGreenLed()
 {
-    led_activateLed(ledColors_Green);
-    return;
-}
-
-void led_resetLeds()
-{
-    int i;
-    for (i = 0; i < NUM_LEDS; i++)
-    {
-        if (HAL_GPIO_ReadPin(ledArray[i].port, ledArray[i].pin) == GPIO_PIN_SET)
-        {
-            HAL_GPIO_WritePin(ledArray[i].port, ledArray[i].pin, GPIO_PIN_RESET);
-        }
-    }
+    led_ColorChange(collArray[collection1], ledColors_Green);
+    led_On(collArray[collection1]);
     return;
 }
--- app/Drivers/inc/led.h
+++ app/Drivers/inc/led.h
@@ -8,8 +8,10 @@
 //     blue
 // } ledColor_t;
 
-void led_activateLed(ledColor_t color);
-void led_resetLeds();
+void led_On(Collection_t coll);
+void led_Off (Collection_t coll);
+void led_Blink (Collection_t coll);
+void led_ColorChange (Collection_t coll, ledColors_t newColor);
 void led_activateRedLed();
 void led_activateBlueLed();
 void led_activateGreenLed();
